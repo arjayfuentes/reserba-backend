9a07f0a619f222a2448100956d8d6530b2a4759e
Arjay Fuentes <arjay630@gmail.com>
2026-03-20 22:22:24 +0100

    added cors

---
+++ src/main/java/com/rjproj/reserba/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.rjproj.reserba.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:4200",        // local dev
+                        "https://reserba.vercel.app",    // your future Vercel URL
+                        "https://reviewer-ariel.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
